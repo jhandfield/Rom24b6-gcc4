67168ba15681fc3cd14c7ccd6b0525793fef1907
Justin Handfield <justin@handfield.org>
2024-02-24 15:18:11 -0500

    Updated source to compile in stock WSL2 Ubuntu 22.04 environment

--- src/db.c
+++ src/db.c
@@ -75,14 +75,11 @@ HELP_DATA *		help_last;
 SHOP_DATA *		shop_first;
 SHOP_DATA *		shop_last;
 
-NOTE_DATA *		note_free;
-
 char			bug_buf		[2*MAX_INPUT_LENGTH];
 CHAR_DATA *		char_list;
 char *			help_greeting;
 char			log_buf		[2*MAX_INPUT_LENGTH];
 KILL_DATA		kill_table	[MAX_LEVEL];
-NOTE_DATA *		note_list;
 OBJ_DATA *		object_list;
 TIME_INFO_DATA		time_info;
 WEATHER_DATA		weather_info;
